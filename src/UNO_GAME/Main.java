a1ba65b0362365c3f29270a770d4f751a0ae02a7
Clayvis Rawlins <Clayvis27@gmail.com>
2022-01-19 13:16:37 -0800

    Made Changes to Player Class and ran tests
    Changes made to Player Class, flushed out Add cards to hand and display

--- src/UNO_GAME/Main.java
+++ src/UNO_GAME/Main.java
@@ -1,11 +1,16 @@
 package UNO_GAME;
-// Another Change
+// Test
 public class Main {
     public static void main(String[] args) {
         Player one = new Player(7);
         Player two = new Player(7);
-
         System.out.println(one);
+        one.displayPlayerHand();
         System.out.println(two);
+        two.displayPlayerHand();
+        one.addCardToHand(7);
+        System.out.println();
+       // System.out.println(one.playerHand);
+
     }
 }
