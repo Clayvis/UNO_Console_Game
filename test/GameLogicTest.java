db7a1c444f6360063091bfce90e785265cee58b9
justinmsuen <jsx616@gmail.com>
2022-01-18 21:54:45 -0500

    GameLogic and Main class

---
+++ test/GameLogicTest.java
@@ -0,0 +1,18 @@
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameLogicTest {
+
+    @Before
+    public void init() {}
+
+
+    @Test
+    public void testStartGame(){
+        GameLogic game = new GameLogic();
+        game.startGame();
+    }
+
+
+
+}
