0bdb34710d1ddc8965f9cf556829b0bb86529705
Clayvis Rawlins <Clayvis27@gmail.com>
2022-01-19 16:23:16 -0800

    Merge branch 'clayR' of C:\Users\clayraw\Documents\GitHub\UNO_Console_Game with conflicts.

--- src/UNO_GAME/Card.java
+++ src/UNO_GAME/Card.java
@@ -1,6 +1,5 @@
 package UNO_GAME;
 
-//Do you see this?
 public class Card
 {
     public static final int RED   = 4;
@@ -9,15 +8,16 @@ public class Card
     public static final int YELLOW = 1;
 
     private static final String[] Suit = { "*", "Yellow", "Green", "Blue", "Red"};
-    private static final String[] Rank = { "0", "1", "2", "3", "4","5", "6", "7", "8", "9"};
+    private static final String[] Rank = { "0", "1", "2", "3", "4",
+            "5", "6", "7", "8", "9"};
 
-    private int cardSuit;
-    private int cardRank;
+    private byte cardSuit;
+    private byte cardRank;
 
     public Card( int suit, int rank )
     {
-        cardRank = rank;
-        cardSuit = suit;
+        cardRank = (byte) rank;
+        cardSuit = (byte) suit;
     }
 
     public int suit()
@@ -46,23 +46,5 @@ public class Card
     public String toString()
     {
         return (  Suit[ cardSuit ] + Rank[ cardRank ]);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Card card = (Card) o;
-
-        if (cardSuit != card.cardSuit) return false;
-        return cardRank == card.cardRank;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = cardSuit;
-        result = 31 * result + cardRank;
-        return result;
     }
 }
